1f8332bc9ce9bdedee3a1c7e88d8bb9e117b53c2
Rishab Sharma <rishusharma.sharma7@gmail.com>
2019-07-26 01:13:25 +0530

    Update test_camera.sh

--- test_camera.sh
+++ test_camera.sh
@@ -2,7 +2,7 @@
 
 python3 test_camera.py \
  	--model /content/model_obj.pth \
- 	--size 320 \
+  --size 320 \
   --image_path /content/clip_img/1803151818/clip_00000000/1803151818-00000007.jpg \
  	# --without_gpu
 
