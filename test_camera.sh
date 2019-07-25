f469631a4076e41fd431e0da4ec41f21d1908017
Rishab Sharma <rishusharma.sharma7@gmail.com>
2019-07-26 01:13:03 +0530

    Update test_camera.sh

--- test_camera.sh
+++ test_camera.sh
@@ -1,7 +1,8 @@
 # test_camera
 
 python3 test_camera.py \
- 	--model ./ckpt/human_matting/model/model_obj.pth \
+ 	--model /content/model_obj.pth \
  	--size 320 \
+  --image_path /content/clip_img/1803151818/clip_00000000/1803151818-00000007.jpg \
  	# --without_gpu
 
